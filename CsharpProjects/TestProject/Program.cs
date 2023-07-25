b2a4a6390f1f8704c01f96c6393e41aa1494644b
Shivani Choudhary <choudhary.shivani489@gmail.com>
2023-07-25 21:08:56 +0300

    some practice programs

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello C#!");
+﻿using System;
+
+class Program 
+{
+     static void Main()
+     {
+        int x= 1;
+        int y = 2;
+        int z = 3;
+        string s = "C";
+
+        if (((x >= y) && (y < z)) || ((x < y) && (s == "C")))
+        {
+        if ( s == "C") 
+          {
+            Console.WriteLine("Success");
+          }
+          else 
+          {
+            Console.WriteLine("Error");
+          }
+        }
+     }
+}
