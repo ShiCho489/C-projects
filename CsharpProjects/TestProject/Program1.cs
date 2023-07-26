60131aae37676d6ce0a13af9057455d83c1cc8d9
Shivani Choudhary <choudhary.shivani489@gmail.com>
2023-07-26 14:29:49 +0300

    program1

---
+++ CsharpProjects/TestProject/Program1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EligibleToVote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ask the user for their age
+            Console.WriteLine("Please input your age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            // Check if the user is eligible to vote
+            if (age >= 18)
+            {
+                Console.WriteLine("You can cast your vote!");
+            }
+           
+        }
+    }
+}
