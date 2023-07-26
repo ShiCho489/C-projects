77adb226fb6f154bc9e2c371f4d4f2d5ac1458a0
Shivani Choudhary <choudhary.shivani489@gmail.com>
2023-07-26 14:50:15 +0300

    progtam2

---
+++ CsharpProjects/TestProject/Program2.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace EligibleToVote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ask the user to input two integers
+            Console.WriteLine("Please input the first number: ");
+            int firstNumber = int.Parse(Console.ReadLine());
+             Console.WriteLine("Please input the second number: ");
+            int secondNumber = int.Parse(Console.ReadLine());
+        
+        }
+    
+
+     
+
+     if ()
+    }
+}
