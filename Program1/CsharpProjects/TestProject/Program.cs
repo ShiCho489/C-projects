ff52a4942cfc4d18296f87c58b9a416aa620807f
Shivani Choudhary <choudhary.shivani489@gmail.com>
2023-07-27 10:57:17 +0300

    package

--- Program1/CsharpProjects/TestProject/Program.cs
+++ Program1/CsharpProjects/TestProject/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using System;
+
+namespace WhileProgram 
+{
+    class Program 
+    {
+        static void Main()
+        {
+            int i = 0;
+            while (i < 5)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+        }
+    }
+}
