5480491285261ee40022efd639ab4e93180da5e0
Shivani Choudhary <choudhary.shivani489@gmail.com>
2023-07-27 13:06:02 +0300

    cockroch-foot-nuke-game

--- Program1/CsharpProjects/TestProject/Program.cs
+++ Program1/CsharpProjects/TestProject/Program.cs
@@ -1,17 +1,50 @@
 ﻿using System;
 
-namespace WhileProgram 
+namespace CockroachFootNuke
 {
-    class Program 
+    class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
-            int i = 0;
-            do
+            // Define the three possible choices
+            string[] choices = { "Cockroach", "Foot", "Nuke" };
+
+            // Generate the computer's choice
+            int computer = new Random().Next(1, 4);
+            string computerChoice = choices[computer - 1];
+
+            // Get the user's choice
+            string userChoice = Console.ReadLine();
+
+            // Compare the two choices
+            if (userChoice == computerChoice)
             {
-                Console.WriteLine(i);
-                i++;
-            }   while (i < 5);
+                Console.WriteLine("Tie!");
+            }
+            else if (userChoice == "Cockroach" && computerChoice == "Foot")
+            {
+                Console.WriteLine("You lose! Foot crushes cockroach.");
+            }
+            else if (userChoice == "Cockroach" && computerChoice == "Nuke")
+            {
+                Console.WriteLine("You win! Cockroach survives nuke.");
+            }
+            else if (userChoice == "Foot" && computerChoice == "Nuke")
+            {
+                Console.WriteLine("You lose! Nuke destroys foot.");
+            }
+            else if (userChoice == "Foot" && computerChoice == "Cockroach")
+            {
+                Console.WriteLine("You win! Foot stomps cockroach.");
+            }
+            else if (userChoice == "Nuke" && computerChoice == "Cockroach")
+            {
+                Console.WriteLine("You lose! Nuke kills cockroach.");
+            }
+            else
+            {
+                Console.WriteLine("You win! Nuke destroys foot.");
+            }
         }
     }
 }
